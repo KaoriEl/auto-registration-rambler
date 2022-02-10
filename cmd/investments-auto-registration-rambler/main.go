1082d226221c2fd6ba5aaa38842e33dcfd6a977e
Rostislav Krivets <krivets.rostislav@yandex.ru>
2022-02-10 17:01:06 +0700

    [UPD] clear main.go

--- cmd/investments-auto-registration-rambler/main.go
+++ cmd/investments-auto-registration-rambler/main.go
@@ -3,29 +3,5 @@ package main
 import "main/internal/server"
 
 func main() {
-	//initEnv()
-	//registration.Registration()
 	server.Serve()
-
-	//reqBody, err := json.Marshal(map[string]string{
-	//	"text":           "12313",
-	//	"cid":            "213",
-	//	"need_send_file": "false",
-	//	"type_message":   "default_message",
-	//})
-	//if err != nil {
-	//	print(err)
-	//}
-	//
-	//resp, err := http.Post("http://investments-api-ms-nginx/api/v1/rambler/register/password",
-	//	"application/x-www-form-urlencoded", bytes.NewBuffer(reqBody))
-	//if err != nil {
-	//	print(err)
-	//}
-	//defer resp.Body.Close()
-	//body, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	print(err)
-	//}
-	//fmt.Println(string(body))
 }
